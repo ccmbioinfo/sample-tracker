a2173c899dcf14e484c33cc9c65d3bf83a8e7ac5
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-02-04 17:45:43 -0500

    Add create/update user endpoints

--- app/admin_routes.py
+++ app/admin_routes.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import abort
+from flask import abort, Response
 from flask_login import current_user, login_required
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, validators
@@ -14,23 +14,76 @@ class UserForm(FlaskForm):
 	username = StringField('Username', [validators.Length(min=4, max=30)])
 	email = StringField('Email')
 	isAdmin = BooleanField('Admin?')
-	password = PasswordField('Password', [validators.Length(min=4)])
-	confirmPassword = PasswordField('Confirm password', [validators.Length(min=4)])
+	password = PasswordField('Password')
+	confirmPassword = PasswordField('Confirm password')
 
 
 @app.route('/admin/users', methods=['GET'])
 @login_required
 def user_list():
-	if current_user.accessLevel == AccessLevel.Admin:
-		db_users = db.session.query(User).all()
-		users = [
-			{
-				'username': user.username,
-				'email': user.email,
-				'accessLevel': user.accessLevel.value
-			}
-			for user in db_users
-		]
-		return json.dumps(users)
-	abort(401)
+	if current_user.accessLevel != AccessLevel.Admin:
+		return abort(401)
 
+	db_users = db.session.query(User).all()
+	users = [
+		{
+			'username': user.username,
+			'email': user.email,
+			'accessLevel': user.accessLevel.value
+		}
+		for user in db_users
+	]
+	return json.dumps(users)
+
+
+@app.route('/admin/users', methods=['POST'])
+@login_required
+def create_update_user():
+	if current_user.accessLevel != AccessLevel.Admin:
+		return abort(401)
+
+	form = UserForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data)
+		if user is None:
+			return create_user(form)
+		return update_user(user, form)
+	else:
+		return abort(400)
+
+
+def create_user(form: UserForm):
+	if form.password.data and len(form.password.data) >= 4 and form.password.data == form.confirmPassword.data:
+		user = User(
+			username=form.username.data,
+			email=form.email.data,
+			password=generate_password_hash(form.password.data),
+			accessLevel=AccessLevel.Admin if form.isAdmin.data else AccessLevel.Regular
+		)
+		db.session.add(user)
+		try:
+			db.session.commit()
+			return Response(status=201)
+		except:
+			db.session.rollback()
+			return abort(500)
+	else:
+		return abort(400)
+
+
+def update_user(user: User, form: UserForm):
+	if form.password.data:
+		if len(form.password.data) >= 4 and form.password.data == form.confirmPassword.data:
+			user.update({'password': generate_password_hash(form.password.data)})
+		else:
+			return abort(400)
+	user.update({
+		'email': form.email,
+		'accessLevel': AccessLevel.Admin if form.isAdmin.data else AccessLevel.Regular
+	})
+	try:
+		db.session.commit()
+		return Response(status=201)
+	except:
+		db.session.rollback()
+		return abort(500)
