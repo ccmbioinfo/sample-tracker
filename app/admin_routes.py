09f8e7a07ce6f5725d0a54901271e806b6293dbe
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-02-04 16:17:09 -0500

    Add UserForm for create/update operations on User

--- app/admin_routes.py
+++ app/admin_routes.py
@@ -2,15 +2,25 @@ import json
 
 from flask import abort
 from flask_login import current_user, login_required
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, StringField, validators
 from werkzeug.security import generate_password_hash
 
 from app import app, db
 from app.models import User, AccessLevel
 
 
-@app.route('/admin')
+class UserForm(FlaskForm):
+	username = StringField('Username', [validators.Length(min=4, max=30)])
+	email = StringField('Email')
+	isAdmin = BooleanField('Admin?')
+	password = PasswordField('Password', [validators.Length(min=4)])
+	confirmPassword = PasswordField('Confirm password', [validators.Length(min=4)])
+
+
+@app.route('/admin/users', methods=['GET'])
 @login_required
-def admin():
+def user_list():
 	if current_user.accessLevel == AccessLevel.Admin:
 		db_users = db.session.query(User).all()
 		users = [
