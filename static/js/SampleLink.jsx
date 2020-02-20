1bfcc8bffd8335e277033b30e6ab1f6e890abeb8
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-02-20 11:45:20 -0500

    Fix participant info modal often not appearing + reformat

--- static/js/SampleLink.jsx
+++ static/js/SampleLink.jsx
@@ -1,43 +1,33 @@
 import React from "react";
 import SampleDetailBox from "./SampleDetailBox";
-import {Modal} from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 
-export default class SampleLink extends React.Component{
-
-    constructor(props){
-
+export default class SampleLink extends React.Component {
+    constructor(props) {
         super(props);
-        this.displaySampleBox = this.displaySampleBox.bind(this);
-		this.closeModal = this.closeModal.bind(this);
-        this.state = { show: false };
-
+        this.state = {show: false};
     }
-    displaySampleBox(){
-    
-        this.setState({ show: true });
 
-
-    }
-	closeModal(){
-
-	    this.setState({ show: false });
-
-	}
-    render(){
-
-        return(
-
-			<div>
-			<Modal {...this.props} bsSize="large" aria-labelledby="Sample_history" show={this.state.show} onHide={this.closeModal}>
-				<Modal.Header closeButton>
-            				<Modal.Title id='Sample_history'>Sample history and details</Modal.Title>
-          			</Modal.Header>
-          			<Modal.Body>
-					<SampleDetailBox sampleData={this.props.sampledetails} />
-				</Modal.Body>
-				</Modal>
-                        <a href='#' onClick={this.displaySampleBox}>{this.props.sampledetails.Sample}</a>
-			</div>
+    render() {
+        return (
+            <div>
+                <Modal {...this.props} bsSize="large" aria-labelledby="Sample_history"
+                       show={this.state.show}
+                       onHide={() => this.setState({show: false})}>
+                    <Modal.Header closeButton>
+                        <Modal.Title id='Sample_history'>Sample history and details</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <SampleDetailBox sampleData={this.props.sampledetails}/>
+                    </Modal.Body>
+                </Modal>
+                <a href='#' onClick={e => {
+                    e.preventDefault();
+                    this.setState({show: true});
+                }}>
+                    {this.props.sampledetails.Sample}
+                </a>
+            </div>
         );
     }
 }
